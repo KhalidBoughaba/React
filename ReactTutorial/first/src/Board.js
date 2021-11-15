b10d9acb62a5d09af1812b18629e67df55eb6279
KhalidBoughaba <boughaba.khalid.dev@gmail.com>
2021-11-15 17:02:52 +0100

    save

--- ReactTutorial/first/src/Board.js
+++ ReactTutorial/first/src/Board.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import Square from './Square'
+import React from "react";
+import Square from "./Square";
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
   renderSquare(i) {
-    return <Square value={i} />
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next Player : X'
+    const status = "Next Player : X";
     return (
       <div>
         <div className="status">{status}</div>
@@ -27,8 +43,8 @@ class Board extends React.Component {
           {this.renderSquare(8)}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Board
+export default Board;
