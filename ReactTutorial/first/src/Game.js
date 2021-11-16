73720aebda3cf78538f61479693f76585e3fa55f
KhalidBoughaba <boughaba.khalid.dev@gmail.com>
2021-11-16 17:01:54 +0100

    save

--- ReactTutorial/first/src/Game.js
+++ ReactTutorial/first/src/Game.js
@@ -1,19 +1,42 @@
-import React from 'react'
-import Board from './Board'
+import React from "react";
+import Board from "./Board";
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(nul),
+        },
+      ],
+      xIsNext: true,
+    };
+  }
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
     return (
       <main className="game">
         <section className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </section>
         <section className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </section>
       </main>
-    )
+    );
   }
 }
-export default Game
+export default Game;
